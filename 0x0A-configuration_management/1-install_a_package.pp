580d0b6465e4c1f871eef8205a46c931b4f9b627
Wisdom Sena <wisdomsena36@gmail.com>
2023-12-22 12:58:57 -0800

    Fix 1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -3,8 +3,9 @@ package { 'python3-pip':
   ensure => installed,
 }
 
-package { 'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
-  require  => Package['python3-pip'],
+exec { 'install_flask':
+  command => '/usr/bin/pip3 install flask==2.1.0',
+  path    => ['/usr/bin'],
+  unless  => '/usr/bin/pip3 show flask | grep Version | grep -q 2.1.0',
+  require => Package['python3-pip'],
 }
