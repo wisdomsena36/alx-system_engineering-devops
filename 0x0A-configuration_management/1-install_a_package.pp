b83e95e1514ff0803269b342276afa0b1aad2db4
Wisdom Sena <wisdomsena36@gmail.com>
2023-12-22 13:57:53 -0800

    Fix 1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,5 @@
-# Install puppet-lint package
-package { 'puppet-lint':
-  ensure   => '2.1.1',
-  provider => 'gem',
+# Install flask package
+package { '/usr/bin/flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
 }
